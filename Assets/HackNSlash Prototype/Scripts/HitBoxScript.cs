6b2ed81f8a7318970591616367be9773980a616e
Livio <liviolunin@gmail.com>
2016-07-11 18:27:20 +0200

    hitboxscript updatet

---
+++ Assets/HackNSlash Prototype/Scripts/HitBoxScript.cs
@@ -0,0 +1,41 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class HitBoxScript : MonoBehaviour {
+
+	HitterScript parentHitterScript;
+
+	// Use this for initialization
+	void Start () {
+		if (transform.parent) SetParentHitterScript(GetComponentInParent<HitterScript>());
+	}
+
+	public void SetParentHitterScript(HitterScript inS){
+		parentHitterScript = inS;
+		Debug.Log("set hitterscript: "+parentHitterScript + "self: "+name);
+
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+	void OnTriggerEnter(Collider inC){
+		DestructibleScript dS = inC.GetComponent<DestructibleScript>();
+		if(dS){
+
+			if(!parentHitterScript){
+				Debug.Log("hitterscript: "+parentHitterScript + "self: "+name+" self.parent: "+transform.parent);
+				Debug.Log("hitterscript: "+parentHitterScript + "self: "+name+" self.parent: "+transform.parent.name);
+			}
+			if(parentHitterScript.HitsDestructible(dS)){
+				if(GetComponent<ProjectileScript>()){
+					Destroy(gameObject);
+				}
+			}
+
+
+		}
+	}
+}
