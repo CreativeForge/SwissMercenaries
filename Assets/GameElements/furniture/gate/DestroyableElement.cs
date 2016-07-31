9bb2a685969703292aeba9c038b125e8d6c2b60f
ixistenz <rene.bauer@zhdk.ch>
2016-07-31 23:56:37 +0200

    [GameElements] Simple destroyable Gate (with Bug?)

---
+++ Assets/GameElements/furniture/gate/DestroyableElement.cs
@@ -0,0 +1,30 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class DestroyableElement : GameElementBased {
+
+	public GameObject[] arrElements; // destroy first > end 0-x
+
+	int index = 0;
+
+	public override void OnPlayerHit() {
+
+		Debug.Log("DestroyableElement.OnPlayerHit() ");
+
+		if (gameElement!=null) {
+			if (index<arrElements.Length) {
+				// wood explosion
+				AddNotification( "visual.woodexplosion", "self", 0.0f, "" );
+				GameObject obj = arrElements[index];
+				obj.SetActive(false);
+				// add on
+				index++;
+			} else {
+				// deactivate ... 
+			}
+		
+		}
+	}
+
+}
+
