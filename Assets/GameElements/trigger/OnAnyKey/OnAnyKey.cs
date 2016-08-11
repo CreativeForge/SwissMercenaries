c66ef603bf9556dcfec1f75aeb4ea946f3b20c6b
ixistenz <rene.bauer@zhdk.ch>
2016-08-11 13:55:15 +0200

    [GameElement] OnAnyKey ... Jump to the next level

---
+++ Assets/GameElements/trigger/OnAnyKey/OnAnyKey.cs
@@ -0,0 +1,16 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class OnAnyKey : GameElementBased {
+
+
+	
+	// Update is called once per frame
+	void Update () {
+		// any key
+		if (Input.anyKeyDown) {
+			// load next level ...
+			gameLogic.LoadGameNextLevel();
+		}
+	}
+}
