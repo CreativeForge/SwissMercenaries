af3288ffa10afd3e765d0443e7c005ceed336312
ixistenz <rene.bauer@zhdk.ch>
2016-08-16 01:24:45 +0200

    [GameElements] GUIMenu implemented (loadlevel,http://,exit)

--- Assets/GameElements/guimenu/menu/GUIMenu.cs
+++ Assets/GameElements/guimenu/menu/GUIMenu.cs
@@ -2,55 +2,74 @@
 using System.Collections;
 using UnityEngine.UI;
 
+/*
+ * GUIMenu (argument > splitted)
+ *  |_GUIMenuPoint
+ *  |_GUIMenuPoint
+ *  |_GUIMenuPoint
+ *  |_GUIMenuPoint
+ * 
+ * */
+
 public class GUIMenu : GameElementBased {
 
 	bool started = false;
 
 	public Canvas menuCanvas;
 
-	public GameObject menuPoint;
+	public GameObject menuPointPrefab;
 
 	string[] arrMenuPoints;
 	string[] arrTexts;
-	string[] arrTargets;
+	string[] arrCommands;
 
+	// ... take first ...
 	string hoveredMenupoint = "";
 
 	// mouseover or ! selection
-	void HoverMenuPoint( string keypoint ) {
+	/*
+	 * void HoverMenuPoint( string keypoint ) {
+
+		// disable all the others
+
+		// show special ...
+
 
 	}
+	*/
 	// activate menupoint
-	void ActivateMenuPoint( string keypoint ) {
+	/*
+	 * void ActivateMenuPoint( string keypoint ) {
 		
 	}
-
+	*/
 
 	ArrayList arrButtons = new ArrayList();
 
 	// Update is called once per frame
 	void FixedUpdate () {
-
+		bool debugThis = false;
 		if (gameElement!=null) {
 			if (!started) {
 				started = true;
 				// ADD IT
-				if (menuPoint!=null) {
-					Debug.Log("GUIMenu.FixedUpdate() // Create new ...");
+				if (menuPointPrefab!=null) {
+					if (debugThis) Debug.Log("GUIMenu.FixedUpdate() // Create new ...");
 
 
 					// targets
 					arrMenuPoints = gameElement.argument.Split(',');
 					arrTexts = gameElement.argument.Split(',');
-					arrTargets = gameElement.argumentsub .Split(',');
+					arrCommands = gameElement.argumentsub .Split(',');
 
 					float startPointX = -200.0f;
 					float startPointY = 140.0f;
 					for (int z=0;z<arrMenuPoints.Length;z++) {
 						// do it
 						string strMenupoint = arrMenuPoints[z];
-						GameObject mp=Instantiate(menuPoint, new Vector3(0.0f,0.0f,0.0f), new Quaternion()) as GameObject;
-						GUIMenuPoint gmp = mp.GetComponent<GUIMenuPoint>();
+						if (debugThis) Debug.Log("GUIMenu.FixedUpdate() // CREATE MENUPOINTS "+strMenupoint);
+						GameObject mp=Instantiate(menuPointPrefab, new Vector3(0.0f,0.0f,0.0f), new Quaternion()) as GameObject;
+						GUIMenuPoint gmp = mp.GetComponent<GUIMenuPoint>(); // button
 						RectTransform rt = mp.GetComponent<RectTransform>();
 						rt.anchoredPosition = new Vector3(startPointX, startPointY);
 						// .SetSize(size);
@@ -61,9 +80,13 @@ public class GUIMenu : GameElementBased {
 						// arrButtons
 						arrButtons.Add(mp);
 
+						// listener
+						Button buttonScript = mp.GetComponent<Button>();
+						buttonScript.onClick.AddListener(() => ButtonAction(""+strMenupoint));
+
 						startPointY = startPointY - 70.0f;
 
-
+						// 
 					}
 				}
 
@@ -72,4 +95,94 @@ public class GUIMenu : GameElementBased {
 			}
 		}
 	}
+
+
+	// destroy all ...
+	void Destroy()
+	{
+		
+		if (gameElement!=null) {
+			if (!started) {
+				started = true;
+				// ADD IT
+				if (menuPointPrefab!=null) {
+					for (int z=0;z<arrMenuPoints.Length;z++) {
+						string strMenupoint = arrMenuPoints[z];
+						GameObject mp = (GameObject) arrButtons[z];
+						Button buttonScript = mp.GetComponent<Button>();
+						buttonScript.onClick.RemoveListener(() => ButtonAction(""+strMenupoint));
+					}
+				}
+			}
+		}
+	}
+
+	string SearchCommandMenuPoint ( string menupointKey ) {
+		// Debug.Log("GUIMenu.SearchCommandMenuPoint("+menupointKey+") // arrCommands.Length = "+arrCommands.Length);
+		for (int z=0;z<arrMenuPoints.Length;z++) {
+			string strMenupoint = arrMenuPoints[z];
+			if (menupointKey.Equals(strMenupoint)) {
+				if (z<arrCommands.Length) {
+					return arrCommands[z];
+				}
+			}
+		}
+
+		return null;
+	}
+
+	void ButtonAction( string menupoint )
+	{
+		Debug.Log("GUIMenu.ButtonAction()"+menupoint);
+
+		// command
+		string command = SearchCommandMenuPoint( menupoint );
+		if (command==null) {
+			Debug.LogError("LevelEditor.GUIMEnu.ButtonAction("+menupoint+" > "+command+") // NOT FOUND!!");
+		}
+		if (command!=null) {
+
+		// search menupoint now
+		bool parsed = false;
+
+		// LEVELID
+		// parse now
+		try {
+			int levelToLoad = int.Parse(command);
+			parsed = true;
+			// gameLogic.levelEditor.LoadLevel(levelToLoad);
+			gameLogic.SetGameLevel(levelToLoad);
+		} catch {
+			
+		}
+
+		// WEB
+		// case: http://
+		if (!parsed) {
+			if (command.IndexOf("http://")!=-1) {
+					Application.OpenURL(""+command);	
+					parsed = true;
+			}
+		}
+
+		// EXIT-COMMAND
+		if (!parsed) {
+			if (command.Equals("exit")) {
+				Application.Quit()	;
+				parsed = true;
+			}
+		}
+
+		// case: notification
+		// it is a notification!
+		if (!parsed) {
+			// LOG
+			Debug.LogError("LevelEditor.GUIMEnu.ButtonAction("+menupoint+" > "+command+") // NOTIFICATION ");
+			
+		}
+		
+		}
+
+	}
+
 }
--- Assets/GameElements/guimenu/menu/GUIMenuPoint.cs
+++ Assets/GameElements/guimenu/menu/GUIMenuPoint.cs
@@ -1,14 +1,43 @@
 ﻿using UnityEngine;
 using System.Collections;
 using UnityEngine.UI;
+using UnityEngine.Events;
+using UnityEngine.EventSystems;
 
 public class GUIMenuPoint : MonoBehaviour {
 
 	public Text scriptText; 
+	// listeners in GUIMenu
 	 
+    // SetText
 	public void SetText( string newrtftext ) {
 		if (scriptText!=null) {
 			scriptText.text = newrtftext;
 		}
 	}
+
+	/*
+	 * OnMouseOver
+	 * 
+	 * */
+/*
+	void Start() {
+		InitCallBacks(UnityAction<GameObject> Enter, UnityAction<GameObject> Exit);
+	}
+
+	UnityAction<GameObject> OnPointerEnter;
+	UnityAction<GameObject> OnPointerExit;
+
+	public void InitCallBacks(UnityAction<GameObject> Enter, UnityAction<GameObject> Exit){
+		OnPointerEnter = Enter;
+		OnPointerExit = Exit;
+	}
+
+	void IPointerEnterHandler.OnPointerEnter (PointerEventData eventData){
+		OnPointerEnter(this.gameObject);
+	}
+	void IPointerExitHandler.OnPointerExit (PointerEventData eventData){
+		OnPointerExit(this.gameObject);
+	}
+*/
 }
