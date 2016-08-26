e08a8335a017e0426041d741497a5fb49eceb215
ixistenz <rene.bauer@zhdk.ch>
2016-08-26 23:56:06 +0200

    [GameElements] Interval spears

---
+++ Assets/GameElements/enemy/spears/TimedSpear.cs
@@ -0,0 +1,35 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class TimedSpear : GameElementBasedInterval {
+
+	public GameObject spear;
+	public Vector3 position = new Vector3();
+
+	void Start() {
+		SetIntervalTime(1.0f);
+	}
+
+	public override void OnInterval() {
+		// ok take ... it
+		if (FirstInterval()) {
+			position.x = spear.transform.localPosition.x;
+			position.y = spear.transform.localPosition.y;
+			position.z = spear.transform.localPosition.z;
+		} 
+
+		if (intervalCounter%2==0) {
+			// inside
+			SetPosition(0.0f);
+		}
+		if (intervalCounter%2==1) {
+			// inside
+			SetPosition(4.0f);
+		}
+
+	}
+
+	void SetPosition( float post ) {
+		spear.transform.localPosition = new Vector3( 0.0f,  0.0f, position.z + post );
+	}
+}
---
+++ Assets/LevelEditor/Classes/GameElementBasedInterval.cs
@@ -0,0 +1,40 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class GameElementBasedInterval : GameElementBased {
+
+	float interval = 4.0f;
+	public void SetIntervalTime( float iinterval ) {
+		interval = iinterval;
+	}
+	float timeForNextInterval = 0.0f;
+
+	public int intervalCounter = 0;
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+		if (Time.time>timeForNextInterval) {
+			timeForNextInterval = Time.time + interval;
+			OnInterval();
+			intervalCounter++;
+		}
+
+	}
+
+	public virtual void  OnInterval() {
+		// intervalCounter !!!
+
+	}
+
+	public bool FirstInterval() {
+		if (intervalCounter==0) return true;
+		return false;
+	}
+
+}
