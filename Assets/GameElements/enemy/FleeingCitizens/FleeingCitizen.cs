8dd353637b6d604b6a2a69ef4d8f91b115610748
ixistenz <rene.bauer@zhdk.ch>
2016-09-02 20:05:35 +0200

    [LevelEditor] Material for collider (editor)

---
+++ Assets/GameElements/enemy/FleeingCitizens/FleeingCitizen.cs
@@ -0,0 +1,14 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class FleeingCitizen : GameElementBasedPathFollower {
+
+	public override float FollowSpeed() {
+		return 0.075f;
+	}
+
+	public  override void OnReachWayPoint( GameElement el ) {
+		AddNotificationHere( "visual", "woodexplosion", 0.0f, "", new Vector3());
+	}
+
+}
